1b8194275d40d676aede6bb8dcb4dde15f185d08
Lucas Di Fini <difinile@gmail.com>
2020-09-16 21:14:06 -0400

    commiting changes

---
+++ Assets/Script.js
@@ -0,0 +1,12 @@
+    // Your web app's Firebase configuration
+    var firebaseConfig = {
+        apiKey: "AIzaSyD2QR0ZNX-98_NbTPxjj7S3E4M9t_6-6lI",
+        authDomain: "reportecambio-aac8b.firebaseapp.com",
+        databaseURL: "https://reportecambio-aac8b.firebaseio.com",
+        projectId: "reportecambio-aac8b",
+        storageBucket: "reportecambio-aac8b.appspot.com",
+        messagingSenderId: "787164328795",
+        appId: "1:787164328795:web:54f7a3c547a306bba5e07e"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
